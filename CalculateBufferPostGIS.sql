1bb71244146b24204b803bf8f1a41cc74a14a94c
Ben Spaulding <bspauld@gmail.com>
2016-11-11 15:24:26 -0500

    Update CalculateBufferPostGIS.sql
    updates to comments

--- CalculateBufferPostGIS.sql
+++ CalculateBufferPostGIS.sql
@@ -3,13 +3,13 @@
 	Script: CalculateBufferPostGIS.sql
 
 	Purpose: Using ST_Buffer, calculate a simple buffer around each point. 
-		buff field will be a polygon feature. ST_Buffer takes two values, the input geomerty field,
-		and a float for the buffer radius.  The buffer radius is in the units of the dataset.
-		In this example, the units, decimal degrees, is used.
+		 buff field will be a polygon feature. ST_Buffer takes two values, the input geomerty field,
+		 and a float for the buffer radius.  The buffer radius is in the units of the dataset.
+		 In this example, the units, decimal degrees, is used.
     
-    Script uses data from https://github.com/bspauld/PostGISfortheGeographer example data
+    		 Script uses data from https://github.com/bspauld/PostGISfortheGeographer example data
 
-		For more detailed information about the buffer feature see - http://postgis.net/docs/ST_Buffer.html
+		 For more detailed information about the buffer feature see - http://postgis.net/docs/ST_Buffer.html
 
 	Author: Ben Spaulding
 
