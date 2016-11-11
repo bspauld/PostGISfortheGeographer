6225fa0592764e237fb242634fe097f5d56992de
Ben Spaulding <bspauld@gmail.com>
2016-11-11 10:06:52 -0500

    Create CreatePostGISDB.sql
    First commit to CreatePostGISDB.sql

---
+++ CreatePostGISDB.sql
@@ -0,0 +1,21 @@
+/*
+  Script: CreatePostGISDB.sql
+  
+  Purpose: Create an empty Postgresql database that is spatially enabled when the
+           create extension command is called for postgis and postgis_topology
+
+
+  Author: Ben Spaulding
+  
+*/
+
+
+--Create empty database
+CREATE DATABASE "PostGISforGeographer"
+  WITH ENCODING='UTF8'
+       CONNECTION LIMIT=-1;
+
+ -- Enable PostGIS (includes raster)
+CREATE EXTENSION postgis;
+-- Enable Topology
+CREATE EXTENSION postgis_topology;
